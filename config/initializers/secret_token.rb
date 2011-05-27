0952924aac53ed7ca52f8bcd9fc54796cec497a8
Don VanDemark <caelon@gmail.com>
2011-05-27 15:36:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreSandersbarry::Application.config.secret_token = 'dc492bceed4b698cef2bf893479bbbe596ab752caabaada24920ba0b17293eef7ad781649f5415568abfba04c08fa4f4f44548c9260e340b6eef30330662f687'
